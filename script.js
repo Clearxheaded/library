7585562f122ec33ec8e364347b4cb1cd7fb629e1
Rus Windows <7777x777x7777@gmail.com>
2025-01-22 16:35:48 -0500

    Work on html and script
    - Create a div for displaying book section
    - Edit the script to make a new function for displaying books

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const myLibrary = [];
+document.addEventListener("DOMContentLoaded", () => {
+    const myLibrary = [];
+    const bookContainer = document.querySelector(".book-container");
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info() = function () {
-        if (read == True) {
-            return `${title} by ${author}, ${pages} pages, read`
+    function Book(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.info = function () {
+            if (read == True) {
+                return `${title} by ${author}, ${pages} pages, read`;
+            }
+            return `${title} by ${author}, ${pages} pages, not read yet`;
         }
-        return `${title} by ${author}, ${pages} pages, not read yet`
     }
-}
 
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-}
+    function addBookToLibrary(title, author, pages, read) {
+        // let title = prompt("Enter the title of the book:", "The Great Gastby");
+        // let author = prompt("Enter the author of the book", "F. Scott Fitzgerald");
+        const book = new Book(title, author, pages, read);
+        myLibrary.push(book);
+    }
+
+    // addBookToLibrary("1984", "George Orwell", "120", true);
+    // addBookToLibrary("1984", "George Orwell", "120", false);
+    // console.log(myLibrary);
+
+    function displayBooks() {
+        bookContainer.innerHTML = "";
+    }
+})
